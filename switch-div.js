248aaa345c66548996f4d9c56c309598e99116e5
canicemichael <canicemichael@gmail.com>
2023-03-05 14:43:48 +0100

    chore: modify elements with necessary id to use for event listeners

--- switch-div.js
+++ switch-div.js
@@ -20,7 +20,7 @@ window.onload = () => {
       optionOne: "PITCH IN",
       optionTwo: "SAY YOU FORGOT YOUR WALLET",
       optionThree: "",
-      result: "void",
+      result: "void 1",
       optionOneMoneyAdded: 0,
       optionOneMoneyRemoved: 0,
       optionTwoMoneyAdded: 0,
@@ -33,7 +33,7 @@ window.onload = () => {
       optionOne: "GIVE YOUR CHILD THE $10",
       optionTwo: "KEEP THE $10",
       optionThree: "",
-      result: "void",
+      result: "void 2",
       moneyAdded: 0,
       moneyRemoved: 0,
       optionOneMoneyAdded: 0,
@@ -48,7 +48,7 @@ window.onload = () => {
       optionOne: "FLY TO THE WEDDING ($350)",
       optionTwo: "SAY YOU CAN'T GO",
       optionThree: "",
-      result: "SOME RESULT TEXT",
+      result: "SOME RESULT TEXT 3",
       moneyAdded: 0,
       moneyRemoved: 0,
       optionOneMoneyAdded: 0,
@@ -56,6 +56,141 @@ window.onload = () => {
       optionOneMoneyAdded: 0,
       optionOneMoneyRemoved: 0,
       optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "You've been putting off doing laundry for weeks because you don't have a washer or dryer.",
+      optionOne: "GO TO THE LAUNDROMAT ($30)",
+      optionTwo: "ASK A FRIEND",
+      optionThree: "WEAR DIRTY CLOTHES",
+      result: "SOME RESULT TEXT 4",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 0,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "The local community college is offering an online course in computer science, which could be your ticket to a higher paycheck. But the class costs $200.",
+      optionOne: "SIGN UP ($200)",
+      optionTwo: "MAYBE NEXT SEMESTER",
+      optionThree: "",
+      result: "SOME RESULT TEXT 5",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 0,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "You're sure you weren't speeding, but the cop who gave you a ticket disagrees.",
+      optionOne: "PAY IT OFF ($250)",
+      optionTwo: "CONTEST IT IN COURT",
+      optionThree: "",
+      result: "SOME RESULT TEXT 6",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 250,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 100,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "Your bathroom sink has a small leak that just became a big leak. Your landlord won't return your calls, and you can't put it off any longer.",
+      optionOne: "CALL A PLUMBER ($150)",
+      optionTwo: "TRY TO FIX IT YOURSELF",
+      optionThree: "",
+      result: "SOME RESULT TEXT 7",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 150,
+      optionTwoMoneyAdded: 0,
+      optionTwoMoneyRemoved: 50,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "One of your co-workers has gotten seriously ill. Because your company doesn't offer sick days, everyone is contributing $20 to help her out.",
+      optionOne: "PITCH IN",
+      optionTwo: "SAY YOU FORGOT YOUR WALLET",
+      optionThree: "",
+      result: "SOME RESULT TEXT 8",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 20,
+      optionOneMoneyRemoved: 0,
+      optionTwoMoneyAdded: 0,
+      optionTwoMoneyRemoved: 0,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "You come out of your house to discover that someone has siphoned the gas from your car. And you're already running late for work.",
+      optionOne: "TAKE THE BUS",
+      optionTwo: "CALL IN SICK",
+      optionThree: "ASK A FRIEND FOR A RIDE",
+      result: "SOME RESULT TEXT 9",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 15,
+      optionTwoMoneyAdded: 0,
+      optionTwoMoneyRemoved: 0,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "The repetitive work you do is taking its toll. Your back hurts so badly you can barely stand, and it's only the beginning of your shift.",
+      optionOne: "SUBMIT A WORKER'S COMP CLAIM",
+      optionTwo: "WORK THROUGH THE PAIN",
+      optionThree: "TAKE THE DAY OFF",
+      result: "SOME RESULT TEXT 10",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 0,
+      optionOneMoneyRemoved: 0,
+      optionTwoMoneyAdded: 0,
+      optionTwoMoneyRemoved: 0,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "Your child's class is going on a field trip to the natural history museum. The trip costs $15.",
+      optionOne: "PAY THE MONEY",
+      optionTwo: "DON'T PAY",
+      optionThree: "",
+      result: "SOME RESULT TEXT 11",
+      moneyAdded: 0,
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 15,
+      optionOneMoneyRemoved: 0,
+      optionTwoMoneyAdded: 0,
+      optionTwoMoneyRemoved: 0,
+      optionThreeMoneyAdded: 0,
+      optionThreeMoneyRemoved: 0,
+    },
+    {
+      note: "Two bills are due today.",
+      optionOne: "PAY CELLPHONE BILL ($75)",
+      optionTwo: "PAY CAR INSURANCE ($100)",
+      optionThree: "PAY THEM BOTH ($175)",
+      result: "SOME RESULT TEXT 12",
+      moneyAdded: 0, //some func that will set the amt in the option clicked
+      moneyRemoved: 0,
+      optionOneMoneyAdded: 75,
+      optionOneMoneyRemoved: 0,
+      optionTwoMoneyAdded: 100,
+      optionTwoMoneyRemoved: 0,
+      // optionThreeMoneyAdded: optionOneMoneyAdded + optionTwoMoneyAdded,
       optionThreeMoneyRemoved: 0,
     },
   ];
@@ -131,23 +266,14 @@ window.onload = () => {
 
   const findAPlaceContinue = document.getElementById("findAPlaceContinue");
   const firstHouseQuestion = document.getElementById("first-house-question");
-  findAPlaceContinue.addEventListener("click", function() {
-    firstHouseQuestion.style.display = "block"
+  findAPlaceContinue.addEventListener("click", function () {
+    firstHouseQuestion.style.display = "block";
     findAPlaceResult.style.display = "none";
-  })
-
-
+  });
 
   const template = document.getElementById("template");
   const resultTemp = document.getElementById("result-temp");
   const resultOk = document.getElementById("result-ok");
-
-  // const note = document.getElementById("note");
-  // const question = document.getElementById("question");
-
-  // const optionOne = document.getElementById("option-one");
-  // const optionTwo = document.getElementById("option-two");
-  // const optionThree = document.getElementById("option-three");
 
   const lastPage = document.getElementById("last-page");
 
@@ -199,42 +325,69 @@ window.onload = () => {
     officeTempPrompt.style.display = "none";
   });
 
-  // optionOne.addEventListener("click", function () {
-  //   // resultTemp.style.display = "block";
-  //   // template.style.display = "none";
-  // });
-
-  // optionTwo.addEventListener("click", function () {
-  //   resultTemp.style.display = "block";
-  //   template.style.display = "none";
-  // });
-
-  // optionThree.addEventListener("click", function () {
-  //   resultTemp.style.display = "block";
-  //   template.style.display = "none";
-  // });
-
   resultOk.addEventListener("click", function () {
     resultTemp.style.display = "none";
     template.style.display = "block";
   });
 
-
+  const noteQue = document.getElementById("note-que");
   const optionOneQue = document.getElementById("option-one-que");
   const optionTwoQue = document.getElementById("option-two-que");
   const optionThreeQue = document.getElementById("option-three-que");
-  const firstQuestionResponseResult = document.getElementById("first-question-response-result");
+  const firstQuestionResponseResult = document.getElementById(
+    "first-question-response-result"
+  );
 
-  optionOneQue.addEventListener("click", function() {
+  optionOneQue.addEventListener("click", function () {
     firstHouseQuestion.style.display = "none";
     firstQuestionResponseResult.style.display = "block";
-  })
-  optionTwoQue.addEventListener("click", function() {
+  
+    const index = Math.floor(Math.random() * questions.length);
+    const object = questions[index];
+
+    noteQue.textContent = object.note;
+    optionOneQue.textContent = object.optionOne;
+    optionTwoQue.textContent = object.optionTwo;
+    optionThreeQue.textContent = object.optionThree;
+  });
+  optionTwoQue.addEventListener("click", function () {
     firstHouseQuestion.style.display = "none";
     firstQuestionResponseResult.style.display = "block";
-  })
-  optionThreeQue.addEventListener("click", function() {
+    
+    const index = Math.floor(Math.random() * questions.length);
+    const object = questions[index];
+
+    noteQue.textContent = object.note;
+    optionOneQue.textContent = object.optionOne;
+    optionTwoQue.textContent = object.optionTwo;
+    optionThreeQue.textContent = object.optionThree;
+  });
+  optionThreeQue.addEventListener("click", function () {
     firstHouseQuestion.style.display = "none";
     firstQuestionResponseResult.style.display = "block";
+    
+    const index = Math.floor(Math.random() * questions.length);
+    const object = questions[index];
+
+    noteQue.textContent = object.note;
+    optionOneQue.textContent = object.optionOne;
+    optionTwoQue.textContent = object.optionTwo;
+    optionThreeQue.textContent = object.optionThree;
+  });
+
+  const resultQueContinue = document.getElementById("result-que-continue");
+  resultQueContinue.addEventListener("click", function() {
+  firstQuestionResponseResult.style.display = "none";
+  firstHouseQuestion.style.display = "block";
   })
+
+
+
+
+  // manipulating div to randomly display different question 
+  // and respective results
+  
+
+
+  // workload
 };
