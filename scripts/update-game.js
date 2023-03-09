32725faa3329ff4bee3dc674bc13b9d407e01637
canicemichael <canicemichael@gmail.com>
2023-03-09 15:00:40 +0100

    add: ste job-option and can-u-play id div  to display none

--- scripts/update-game.js
+++ scripts/update-game.js
@@ -22,3 +22,17 @@ eventer(
 );
 
 console.log('all set')
+
+
+
+const proveIt = document.getElementById("prove-it");
+const proveItBtn = document.getElementById("prove-it-btn");
+
+const canYouPlay = document.getElementById("can-u-play");
+const findJobBtn = document.getElementById("find-a-job");
+const exitBtn = document.getElementById("exit-btn");
+
+const jobOption = document.getElementById("job-options");
+const secShift = document.getElementById("sec-shift");
+const joinOur = document.getElementById("join-our");
+const officeTemp = document.getElementById("office-temp");
